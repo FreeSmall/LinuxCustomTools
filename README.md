a0bab7f8370cd2d1007059102d1fcf10cf76847e
zhaoyang.peng <zhaoyang.peng@tcl.com>
2016-05-14 11:56:06 +0800

    Add a automit install script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # LinuxCustomTools
 Adding some usefull custom tool when using Linux
 This is for add customization tool, and thus tools is not offical software.
+
+#[0.1] 2016-5-14
+Adding a automatic script to download and install LinuxCustomTools
+And the way of install, you can use command:
+curl http://bit.ly/1OpDP2o -L > install.sh && ./install.sh
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# This is a install shell for automatic download and set env
+# FreeSmall Added
+# 2016-5-14
+
+app_name='LinuxCustomTools'
+[ -z $LCT_PATH ] && LCT_PATH="$HOME/.lcts"
+[ -z $LCT_URL ] && LCT_URL='https://github.com/FreeSmall/LinuxCustomTools.git'
+[ -z $LCT_BRANCH ] && LCT_BRANCH='master'
+
+msg() {
+    printf '%b\n' "$1" >&2 ## %b is for escape characters
+}
+
+success() {
+    msg "\e[32m[✔]\e[0m $1$2"
+}
+
+error() {
+    msg "\e[31m[✘]\e[0m $1$2"
+    exit 1
+}
+
+lct_sync() {
+    if [ ! -e "$LCT_PATH" ]; then
+        git clone -b "$LCT_BRANCH" "$LCT_URL" "$LCT_PATH"
+        if [ "$?" == '0' ]; then
+            success "Successfully cloned $app_name"
+        else
+            error
+        fi
+    else
+        cd "$LCT_PATH" && git pull origin "$LCT_BRANCH"
+        if [ "$?" == '0' ]; then
+            success "Successfully updated $app_name"
+        else
+            error
+        fi
+    fi
+}
+
+add_path() {
+    sed -i '/\$HOME\/\.lcts/d' $HOME/.bashrc
+    echo "export PATH=\$PATH:\$HOME/.lcts" >> "$HOME/.bashrc"
+}
+
+#------ main--------
+
+lct_sync
+add_path
